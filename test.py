00b04b629ede51c538e11e16411c998141661f21
Maxwelito <maudigie@gmail.com>
2021-12-02 13:32:45 +0100

    page menu de scola avec mechanize cette fois

--- test.py
+++ test.py
@@ -7,15 +7,35 @@ import mechanize
 
 br = mechanize.Browser()
 br.open("https://ent.uca.fr")
+
+br.form = list(br.forms())[0]
+
+br["username"] = "maaudigie"
+br["password"] = passw
+
+response = br.submit()
+soup = BeautifulSoup(response, 'lxml')
+#print(soup.prettify())
+
+response2 = br.open("https://ent.uca.fr/scolarite/stylesheets/etu/welcome.faces")
+soup2 = BeautifulSoup(response2, 'lxml')
+print(soup2.prettify())
+
+print("Les liens \n")
+for link in br.links():
+    print(link.text, link.url)
+
+print("Les forms \n")
 for form in br.forms() :
     print("Form name:", form.name)
     #print(form)
 
-br.form = list(br.forms())[0]
-
+br.form = list(br.forms())[1]
+print("Les controles du form \n")
 for control in br.form.controls:
     #print(control)
     print ("type=%s, name=%s value=%s" % (control.type, control.name, br[control.name]))
+
 '''
 go('https://ent.uca.fr')
 showforms()
