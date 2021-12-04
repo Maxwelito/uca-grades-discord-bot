78fe77313b795ee805fc89ede0eba97fbf037356
Maxwelito <maudigie@gmail.com>
2021-12-05 00:48:17 +0100

    menu des notes mais pas réussi à cliquer sur un lien pour l'instant

--- test.py
+++ test.py
@@ -1,9 +1,11 @@
-from urllib import response
+'''from urllib import response
 import requests
-from bs4 import BeautifulSoup
 from twill.commands import *
-from secret import passw
+'''
+import re
 import mechanize
+from bs4 import BeautifulSoup
+from secret import passw
 
 br = mechanize.Browser()
 br.open("https://ent.uca.fr")
@@ -19,22 +21,49 @@ soup = BeautifulSoup(response, 'lxml')
 
 response2 = br.open("https://ent.uca.fr/scolarite/stylesheets/etu/welcome.faces")
 soup2 = BeautifulSoup(response2, 'lxml')
-print(soup2.prettify())
-
-print("Les liens \n")
-for link in br.links():
-    print(link.text, link.url)
-
+#print(soup2.prettify())
+'''
+print("\n")
 print("Les forms \n")
 for form in br.forms() :
     print("Form name:", form.name)
     #print(form)
 
+print("\n")
 br.form = list(br.forms())[1]
 print("Les controles du form \n")
 for control in br.form.controls:
     #print(control)
     print ("type=%s, name=%s value=%s" % (control.type, control.name, br[control.name]))
+'''
+#lien1 = br.find_link(text_regex=re.compile("Notes"))
+#print(lien1)
+response3 = br.open("https://ent.uca.fr/scolarite/stylesheets/etu/notes.faces")
+soup3 = BeautifulSoup(response3, 'lxml')
+#print(soup3.prettify())
+'''
+print("Les liens \n")
+for link in br.links():
+    print(link.text, link.url)
+print("\n")
+print("Les forms \n")
+for form in br.forms() :
+    print("Form name:", form.name)
+    #print(form)
+
+print("\n")
+br.form = list(br.forms())[1]
+print("Les controles du form \n")
+for control in br.form.controls:
+    #print(control)
+    print ("type=%s, name=%s value=%s" % (control.type, control.name, br[control.name]))
+'''
+lien1 = list(br.links())[12]
+print(lien1)
+request4 = br.click_link(lien1)
+response4 = br.follow_link(lien1)
+soup4 = BeautifulSoup(response4)
+print(soup4.prettify())
 
 '''
 go('https://ent.uca.fr')
