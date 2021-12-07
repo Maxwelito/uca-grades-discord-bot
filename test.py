4187a3204795e4266607e719e00c641a72e93204
Maxwelito <maudigie@gmail.com>
2021-12-07 17:17:35 +0100

    code nettoyé qui arrive sur la page de notes

--- test.py
+++ test.py
@@ -1,4 +1,3 @@
-import re
 import mechanize
 from bs4 import BeautifulSoup
 from secret import passw
@@ -11,14 +10,11 @@ br.form = list(br.forms())[0]
 br["username"] = "maaudigie"
 br["password"] = passw
 
-response = br.submit()
-soup = BeautifulSoup(response, 'lxml')
+br.submit()
 
-response2 = br.open("https://ent.uca.fr/scolarite/stylesheets/etu/welcome.faces")
-soup2 = BeautifulSoup(response2, 'lxml')
+br.open("https://ent.uca.fr/scolarite/stylesheets/etu/welcome.faces")
 
-response3 = br.open("https://ent.uca.fr/scolarite/stylesheets/etu/notes.faces")
-soup3 = BeautifulSoup(response3, 'lxml')
+br.open("https://ent.uca.fr/scolarite/stylesheets/etu/notes.faces")
 
 br.form = list(br.forms())[3]
 br.form.set_all_readonly(False)
@@ -26,9 +22,9 @@ br["_id74:_idcl"] = "_id74:tableetp:0:_id128"
 br["_id74:_link_hidden_"] = "null"
 br['row'] = '4'
 
-response4 = br.submit()
-soup4 = BeautifulSoup(response4, 'lxml')
-print(soup4.prettify())
+response = br.submit()
+soup = BeautifulSoup(response, 'lxml')
+print(soup.prettify())
 
-with open("ziggy.html", "w", encoding = 'utf-8') as file :
-    file.write(str(soup4))
+'''with open("ziggy.html", "w", encoding = 'utf-8') as file :
+    file.write(str(soup4))'''
