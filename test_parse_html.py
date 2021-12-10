2f5d40f04197b1964536581a1e958c99091707cb
Maxwelito <maudigie@gmail.com>
2021-12-10 11:18:40 +0100

    affichage matières + présence d'une note

--- test_parse_html.py
+++ test_parse_html.py
@@ -4,6 +4,12 @@ with open("notes_complet.html") as file :
     soup = BeautifulSoup(file, 'lxml')
 
 tag = soup.tbody
-for i in range (0, len(tag.contents)) :
-    print("item n°", i, tag.contents[i])
 
+for balise in tag.contents :
+    if(balise.name == "tr") :
+        ligne = balise.contents
+        matiere = ligne[2]
+        note = ligne[3]
+        if(len(matiere.contents) == 1) :
+            print(matiere.string)
+            print("note ? long : ", len(note.contents))
