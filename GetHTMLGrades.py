91b6b24a13d4865f2c37cafe0b4ad0b7bbb7036a
Maxwelito <maudigie@gmail.com>
2021-12-14 17:08:05 +0100

    fonctions, récupération du n° de row à faire

--- GetHTMLGrades.py
+++ GetHTMLGrades.py
@@ -1,30 +1,57 @@
 import mechanize
+import re
 from bs4 import BeautifulSoup
 from secret import passw
 
-br = mechanize.Browser()
-br.open("https://ent.uca.fr")
+def GetGradesPage(row) :
+    br = mechanize.Browser()
+    br.open("https://ent.uca.fr")
 
-br.form = list(br.forms())[0]
+    br.form = list(br.forms())[0]
 
-br["username"] = "maaudigie"
-br["password"] = passw
+    br["username"] = "maaudigie"
+    br["password"] = passw
 
-br.submit()
+    br.submit()
 
-br.open("https://ent.uca.fr/scolarite/stylesheets/etu/welcome.faces")
+    br.open("https://ent.uca.fr/scolarite/stylesheets/etu/welcome.faces")
 
-br.open("https://ent.uca.fr/scolarite/stylesheets/etu/notes.faces")
+    br.open("https://ent.uca.fr/scolarite/stylesheets/etu/notes.faces")
 
-br.form = list(br.forms())[3]
-br.form.set_all_readonly(False)
-br["_id74:_idcl"] = "_id74:tableetp:0:_id128"
-br["_id74:_link_hidden_"] = "null"
-br['row'] = '0'
+    br.form = list(br.forms())[3]
+    br.form.set_all_readonly(False)
+    br["_id74:_idcl"] = "_id74:tableetp:0:_id128"
+    br["_id74:_link_hidden_"] = "null"
+    br['row'] = str(row)
 
-response = br.submit()
-soup = BeautifulSoup(response, 'lxml')
-#print(soup.prettify())
+    response = br.submit()
+    soup = BeautifulSoup(response, 'lxml')
 
-with open("notes_vide.html", "w", encoding = 'utf-8') as file :
-    file.write(str(soup))
+    with open("notes_vide.html", "w", encoding = 'utf-8') as file :
+        file.write(str(soup))
+
+    br.close()
+
+def GetRowFormation(formation) :
+    br = mechanize.Browser()
+    br.open("https://ent.uca.fr")
+
+    br.form = list(br.forms())[0]
+
+    br["username"] = "maaudigie"
+    br["password"] = passw
+
+    br.submit()
+
+    br.open("https://ent.uca.fr/scolarite/stylesheets/etu/welcome.faces")
+    br.open("https://ent.uca.fr/scolarite/stylesheets/etu/notes.faces")
+
+    link = br.find_link(text_regex=re.compile(formation))
+    print(link)
+
+    br.close()
+
+    
+
+f = "2ème année DI Informatique"
+GetRowFormation(f)
