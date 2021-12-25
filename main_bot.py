a878d98b94b33de195689889e8743fcea267e1ea
Maxwelito <maudigie@gmail.com>
2021-12-25 22:05:17 +0100

    commandes help et next_check ok

--- main_bot.py
+++ main_bot.py
@@ -2,12 +2,13 @@ import discord
 from discord.ext import commands, tasks
 from channel_management import get_channels_list, init_channel_list, add_a_channel
 from secret import tk
-import pickle
+from datetime import datetime
 
 class GradeNotifier(commands.Bot) :
     def __init__(self, command_prefix='!', help_command=None, description=None, **options):
         super().__init__(command_prefix, help_command=help_command, description=description, **options)
         init_channel_list()
+        self.last_check_time = datetime.now()
         self.check_notes.start()
 
     async def on_ready(self):
@@ -15,12 +16,13 @@ class GradeNotifier(commands.Bot) :
         print("------")
         await self.define_status()
 
-    @tasks.loop(seconds=5)
+    @tasks.loop(seconds=15)
     async def check_notes(self):
         channel_list = get_channels_list()
         for id in channel_list :
             channel = self.get_channel(id)
             await channel.send('yo')
+        self.last_check_time = datetime.now()
 
     @check_notes.before_loop
     async def before_check_notes(self):
@@ -37,9 +39,19 @@ bot = GradeNotifier()
 async def salut(ctx) :
     await ctx.send("salut")
 
-@bot.command()
+@bot.command(brief = 'Add this channel to receive the notifications here')
 async def add_channel(ctx) :
     add_a_channel(ctx.channel.id)
     await ctx.send(f"Le channel {ctx.channel} a été ajouté.")
+
+@bot.command(brief = 'Show when is the next check')
+async def next_check(ctx) :
+    time_now = datetime.now()
+    diff = time_now - bot.last_check_time
+    diff_next = 300 - diff.total_seconds()
+    time_left = divmod(diff_next, 60)
+    await ctx.send(f"Next check in {round(time_left[0])} minutes and {round(time_left[1])} seconds")
+
+bot.help_command = commands.DefaultHelpCommand()
 
 bot.run(tk)
